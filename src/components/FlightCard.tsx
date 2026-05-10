486674057e70d3f1ea2cce082e6a10a33abbf1f1
Camilo <camilolon6@gmail.com>
2026-05-10 12:55:31 -0500

    feat: remove unused status styles and update FlightsPage to use new SearchBar

--- src/components/FlightCard.tsx
+++ src/components/FlightCard.tsx
@@ -8,12 +8,6 @@ interface Props {
   flight: Flight;
 }
 
-const statusStyles: Record<string, string> = {
-  SCHEDULED: "bg-blue-100 text-blue-700",
-  COMPLETED: "bg-green-100 text-green-700",
-  CANCELLED: "bg-red-100 text-red-700",
-};
-
 export const FlightCard = ({ flight }: Props) => {
   const departureTime = flight.localDepartureDateTime.slice(11, 16);
   const arrivalTime = getArrivalTime(
--- src/pages/FlightsPage.tsx
+++ src/pages/FlightsPage.tsx
@@ -1,27 +1,43 @@
 import { useEffect, useState } from "react";
-import { searchFlights } from "../services/flightService";
-import type { CleanFilters, Flight, FlightSearchParams } from "../types/flight";
-import { FlightCard } from "../components/FlightCard";
 import { useNavigate, useSearchParams } from "react-router-dom";
+
+import { searchFlights } from "../services/flightService";
+import {
+  getAvailableDestinations,
+  getAvailableOrigins,
+} from "../services/airportService";
+
+import type { CleanFilters, Flight, FlightSearchParams } from "../types/flight";
+
+import type { AirportSearchOption } from "../types/airport";
+
+import { FlightCard } from "../components/FlightCard";
 import { SearchBar } from "../components/search/SearchBar";
 import { LoadingScreen } from "../components/LoadingScreen";
 import { ErrorScreen } from "../components/ErrorScreen";
-import { getAvailableOrigins } from "../services/airportService";
-import type { AirportSearchOption } from "../types/airport";
 
 export const FlightsPage = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+
   const [flights, setFlights] = useState<Flight[]>([]);
+  const [origins, setOrigins] = useState<AirportSearchOption[]>([]);
+  const [destinations, setDestinations] = useState<AirportSearchOption[]>([]);
+
+  const [originInput, setOriginInput] = useState("");
+  const [destinationInput, setDestinationInput] = useState("");
+
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [hasSearched, setHasSearched] = useState<boolean>(false);
+
   const [filters, setFilters] = useState<FlightSearchParams>({
     origin: "",
     destination: "",
     date: "",
     status: "",
   });
+
   const loadOrigins = async () => {
     try {
       const data = await getAvailableOrigins();
@@ -32,10 +48,15 @@ export const FlightsPage = () => {
     }
   };
 
-  const [origins, setOrigins] = useState<AirportSearchOption[]>([]);
-  useEffect(() => {
-    loadOrigins();
-  }, []);
+  const loadDestinations = async (originCode: string) => {
+    try {
+      const data = await getAvailableDestinations(originCode);
+
+      setDestinations(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleSearch = async (customFilters: FlightSearchParams) => {
     try {
@@ -45,7 +66,9 @@ export const FlightsPage = () => {
       const cleanFilters: CleanFilters = Object.fromEntries(
         Object.entries(customFilters).filter(([_, value]) => value !== ""),
       );
+
       const data = await searchFlights(cleanFilters);
+
       setFlights(data.data);
       setHasSearched(true);
     } catch (err) {
@@ -56,17 +79,29 @@ export const FlightsPage = () => {
   };
 
   useEffect(() => {
+    loadOrigins();
+  }, []);
+
+  useEffect(() => {
     const origin = searchParams.get("origin") || "";
     const destination = searchParams.get("destination") || "";
     const date = searchParams.get("date") || "";
 
-    const initialFilters = {
+    const initialFilters: FlightSearchParams = {
       origin,
       destination,
       date,
+      status: "",
     };
 
     setFilters(initialFilters);
+
+    setOriginInput(origin);
+    setDestinationInput(destination);
+
+    if (origin) {
+      loadDestinations(origin);
+    }
 
     if (origin && destination && date) {
       handleSearch(initialFilters);
@@ -81,11 +116,17 @@ export const FlightsPage = () => {
       <div className="w-full max-w-4xl sticky top-16 z-20 mb-10">
         <SearchBar
           filters={filters}
-          onChange={setFilters}
           origins={origins}
+          destinations={destinations}
+          originInput={originInput}
+          destinationInput={destinationInput}
+          setOriginInput={setOriginInput}
+          setDestinationInput={setDestinationInput}
+          onChange={setFilters}
+          loadDestinations={loadDestinations}
           onSearch={() => {
             const cleanFilters: Record<string, string> = Object.fromEntries(
-              Object.entries(filters).filter(([_, v]) => v !== ""),
+              Object.entries(filters).filter(([_, value]) => value !== ""),
             );
 
             const params = new URLSearchParams(cleanFilters).toString();
@@ -94,15 +135,21 @@ export const FlightsPage = () => {
           }}
         />
       </div>
+
       <div className="mb-6 flex items-center p-5 gap-2 self-start">
-        <h1 className="text-[25px] font-bold ">
+        <h1 className="text-[25px] font-bold">
           Vuelos disponibles {filters.origin} → {filters.destination}
         </h1>
+
         <p className="text-xl text-gray-400">|</p>
+
         <p className="text-lg text-gray-500 opacity-75">{filters.date}</p>
       </div>
+
       {loading && <LoadingScreen />}
+
       {error && <ErrorScreen message={error} />}
+
       {!loading && !error && !hasSearched && (
         <div className="w-full text-center py-10">
           <p className="text-gray-500 text-lg">
@@ -110,11 +157,12 @@ export const FlightsPage = () => {
           </p>
         </div>
       )}
+
       {!loading && !error && hasSearched && flights.length === 0 && (
         <ErrorScreen message="No se encontraron vuelos con los criterios seleccionados" />
       )}
 
-      <div className=" w-full flex flex-col gap-5">
+      <div className="w-full flex flex-col gap-5">
         {flights.map((flight) => (
           <FlightCard key={flight.id} flight={flight} />
         ))}
